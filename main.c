681bfe3e9a7318a73a04408b2e4edf8109524bcf
Gon2153 <125921214+Gon21513@users.noreply.github.com>
2023-10-27 23:20:55 -0600

    Paso1
    Se agregaron librerias

---
+++ main.c
@@ -0,0 +1,24 @@
+//Luis Pedro Gonzalez 21513
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+
+
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	return 0;
+}
